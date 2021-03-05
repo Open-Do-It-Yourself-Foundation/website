4541d73e94d09b8af46cd98103696f57e4268d31
MD HAMIDUL ISLAM <hamidul@MDs-MacBook-Pro.local>
2021-03-05 10:15:35 +0100

    fixed error and add profile for contributor

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../pages/home/Home';
-import Donation from '../pages/donation/Donation';
-import Contributor from '../pages/contributor/Contributors';
-import Signup from '../pages/signup/Signup';
-import Login from '../pages/login/Login';
-import Nav from '../components/nav';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from '../pages/home/Home'
+import Donation from '../pages/donation/Donation'
+import Contributor from '../pages/contributor/Contributors'
+import Signup from '../pages/signup/Signup'
+import Login from '../pages/login/Login'
+import Nav from '../components/nav'
+import Profile from '../pages/contributor/Profile'
 
 function AppRouter() {
-    return (
-        <BrowserRouter>
-            <Nav />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/donate" component={Donation} />
-                <Route exact path="/contributor" component={Contributor} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/login" component={Login} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/donate" component={Donation} />
+        <Route exact path="/contributor" component={Contributor} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default AppRouter;
+export default AppRouter
--- src/pages/contributor/Contributor.tsx
+++ src/pages/contributor/Contributor.tsx
@@ -1,51 +1,42 @@
-import React from 'react';
-import {
-    Button,
-    Card,
-    CardActionArea,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Typography,
-    makeStyles,
-} from '@material-ui/core';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: 300,
-        boxShadow: theme.shadows[5],
-        marginTop: theme.spacing(3),
-        padding: 20,
-    },
-    media: {
-        height: 300,
-        width: '100%',
-    },
-}));
+  root: {
+    maxWidth: 300,
+    boxShadow: theme.shadows[5],
+    marginTop: theme.spacing(3),
+    padding: 20,
+  },
+  media: {
+    height: 300,
+    width: '100%',
+  },
+}))
 
 export default function Contributor(props: any) {
-    const { name, image, desc } = props;
-    const classes = useStyles();
-    return (
-        <div>
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia className={classes.media} image={image} title={name} />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {desc}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button variant="outlined" size="small" color="secondary">
-                        Resume
-                    </Button>
-                </CardActions>
-            </Card>
-        </div>
-    );
+  const { id, name, img, desc } = props.contributor
+  console.log(props.contributor)
+  const classes = useStyles()
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia className={classes.media} image={img} title={name} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {desc}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Link to={`/profile/${id}`}>Profile</Link>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
